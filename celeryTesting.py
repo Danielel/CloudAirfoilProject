4cbeafa9e9e12818e1f0ce4e84515ad09cf5015f
Danielel <denial93@gmail.com>
2017-10-13 13:07:45 +0200

    working celery distribution

--- celeryTesting.py
+++ celeryTesting.py
@@ -1,9 +1,19 @@
 from celery import Celery
+import time
+
 
 broker_url = 'amqp://acc9:aub1923lksad32@130.239.81.221:5672/acc9-host'
 #app = Celery('celeryTesting', broker='pyamqp://guest@localhost//')
-app = Celery('celeryTesting', backend='amqp', broker=broker_url)
+app = Celery('celeryTesting', backend='rpc://', broker=broker_url,  worker_prefetch_multiplier=1)
+#app = Celery('celeryTesting', broker=broker_url)
 
 @app.task
 def add(x, y):
+    time.sleep(5)
     return x + y
+    
+    #celery flower --broker=amqp://acc9:aub1923lksad32@130.239.81.221:5672/acc9-host//
+@app.task
+def doingSlowWork():
+    time.sleep(10)
+    return "HELLO!\n"
---
+++ queueAdd.py
@@ -0,0 +1,5 @@
+from celeryTesting import add
+
+add.delay(3,7)
+add.delay(3,72)
+add.delay(35,700)
